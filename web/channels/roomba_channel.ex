87050dcaa17432a894d9dab866a30188813ae1e9
Michael Ries <michael@riesd.com>
2015-12-26 21:03:55 -0700

    support drive commands from channel clients

--- web/channels/roomba_channel.ex
+++ web/channels/roomba_channel.ex
@@ -2,4 +2,8 @@ defmodule Recruitbot.RoombaChannel do
   use Phoenix.Channel
 
   def join("roomba", _auth, socket), do: {:ok, socket}
+
+  def handle_in("drive", %{"velocity" => velocity, "radius" => radius}) do
+    send :dj, {:command, Roomba.drive(velocity, radius)}
+  end
 end
