c3ba8001df84a8614dd3271e4e7662dfa089e73f
Michael Ries <michael@riesd.com>
2016-01-04 11:43:08 -0700

    pickup new roombex

--- mix.exs
+++ mix.exs
@@ -34,7 +34,7 @@ defmodule Recruitbot.Mixfile do
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.9"},
       {:cowboy, "~> 1.0"},
-      {:roombex, "~> 0.0.2"},
+      {:roombex, "~> 0.0.4"},
     ]
   end
 end
