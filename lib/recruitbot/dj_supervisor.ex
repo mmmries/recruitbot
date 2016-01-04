f37b37bec66809d0d8cd58b41e5b878de7d73e91
Michael Ries <michael@riesd.com>
2016-01-04 11:43:24 -0700

    add our own sensor checking schedule

--- lib/recruitbot/dj_supervisor.ex
+++ lib/recruitbot/dj_supervisor.ex
@@ -2,6 +2,22 @@ defmodule Recruitbot.DJSupervisor do
   use Supervisor
   require Logger
 
+  @bumper_sensors [
+    :bumper_left,
+    :bumper_right,
+    :light_bumper_left,
+    :light_bumper_left_front,
+    :light_bumper_left_center,
+    :light_bumper_right_center,
+    :light_bumper_right_front,
+    :light_bumper_right,
+  ]
+
+  @battery_sensors [
+    :battery_capacity,
+    :battery_charge,
+  ]
+
   def start_link(dj_opts) do
     Supervisor.start_link(__MODULE__, dj_opts, name: __MODULE__)
   end
@@ -9,6 +25,9 @@ defmodule Recruitbot.DJSupervisor do
   def init(dj_opts) do
     publisher_pid = spawn_link(&publish_roomba_updates/0)
     Process.register(publisher_pid, :publisher)
+
+    :timer.send_interval(33, :dj, {:check_on, @bumper_sensors})
+    :timer.send_interval(200, :dj, {:check_on, @battery_sensors})
 
     dj_opts = Keyword.put(dj_opts, :report_to, publisher_pid)
     children = [
@@ -27,6 +46,6 @@ defmodule Recruitbot.DJSupervisor do
   end
 
   defp sensors_to_map(sensors) do
-    Map.take(sensors, [:bumper_left, :bumper_right, :light_bumper_left, :light_bumper_left_front, :light_bumper_left_center, :light_bumper_right_center, :light_bumper_right_front, :light_bumper_right])
+    Map.take(sensors, @battery_sensors ++ @bumper_sensors)
   end
 end
