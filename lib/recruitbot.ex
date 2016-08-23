397573d5b7654dd7f7f6790baba715d024c8b411
hqmq <michael@riesd.com>
2016-08-22 20:41:10 -0600

    do not open serial port during test suite

--- lib/recruitbot.ex
+++ lib/recruitbot.ex
@@ -1,24 +1,12 @@
 defmodule Recruitbot do
   use Application
+  import Supervisor.Spec, warn: false
 
   # See http://elixir-lang.org/docs/stable/elixir/Application.html
   # for more information on OTP Applications
   def start(_type, _args) do
-    import Supervisor.Spec, warn: false
-
-    dj_opts = Application.get_env(:recruitbot, :dj_opts)
-
-    children = [
-      # Start the endpoint when the application starts
-      supervisor(Recruitbot.Endpoint, []),
-      supervisor(Recruitbot.DJSupervisor, [dj_opts]),
-      worker(Recruitbot.Checkin, []),
-    ]
-
-    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Recruitbot.Supervisor]
-    Supervisor.start_link(children, opts)
+    Supervisor.start_link(children(Mix.env), opts)
   end
 
   # Tell Phoenix to update the endpoint configuration
@@ -26,5 +14,20 @@ defmodule Recruitbot do
   def config_change(changed, _new, removed) do
     Recruitbot.Endpoint.config_change(changed, removed)
     :ok
+  end
+
+  defp children(:test) do
+    [
+      supervisor(Recruitbot.Endpoint, []),
+    ]
+  end
+  defp children(_) do
+    dj_opts = Application.get_env(:recruitbot, :dj_opts)
+
+    [
+      supervisor(Recruitbot.Endpoint, []),
+      supervisor(Recruitbot.DJSupervisor, [dj_opts]),
+      worker(Recruitbot.Checkin, []),
+    ]
   end
 end
