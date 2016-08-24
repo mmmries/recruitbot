b6f0b7029bc63fdff3785c315494abd6df2dc7ce
hqmq <michael@riesd.com>
2016-08-23 20:24:12 -0500

    refactor dead reckoner to split up the logic

--- lib/recruitbot/dead_reckoner.ex
+++ lib/recruitbot/dead_reckoner.ex
@@ -2,34 +2,28 @@ defmodule Recruitbot.DeadReckoner do
   alias Recruitbot.WhereAmI
   @axle_length 235.0
 
-    # lovingly borrowed from http://www.seattlerobotics.org/encoder/200010/dead_reckoning_article.html
   def update(%WhereAmI{}=whereami, %{encoder_counts_left: left, encoder_counts_right: right}) do
     left_diff = left - whereami.encoder_counts_left
     right_diff = right - whereami.encoder_counts_right
-    if left_diff == right_diff do
-      distance = encoder_counts_to_distance(left_diff)
-      x_delta = distance * :math.cos(whereami.heading)
-      y_delta = distance * :math.sin(whereami.heading)
-      %{whereami | x: whereami.x + x_delta, y: whereami.y + y_delta, encoder_counts_left: left, encoder_counts_right: right}
-    else
-      cos_current = :math.cos(whereami.heading)
-      sin_current = :math.sin(whereami.heading)
-      dist_left = encoder_counts_to_distance(left_diff)
-      dist_right = encoder_counts_to_distance(right_diff)
-      right_minus_left = dist_right - dist_left
-      expr1 = @axle_length * (dist_right + dist_left) / 2.0 / (right_minus_left)
-      x_delta = expr1 * (:math.sin(right_minus_left / @axle_length + whereami.heading) - sin_current)
-      y_delta = -1 * expr1 * (:math.cos(right_minus_left / @axle_length + whereami.heading) - cos_current)
-      heading_delta = right_minus_left / @axle_length;
+    updated_whereami(whereami, {left, right}, {left_diff, right_diff})
+  end
 
-      %WhereAmI{
-        x: whereami.x + x_delta,
-        y: whereami.y + y_delta,
-        heading: clamp_heading(whereami.heading + heading_delta),
-        encoder_counts_left: left,
-        encoder_counts_right: right,
-      }
-    end
+  defp updated_whereami(whereami, {count_left, count_right}, {count_diff, count_diff}) do
+    distance = encoder_counts_to_distance(count_diff)
+    x_delta = distance * :math.cos(whereami.heading)
+    y_delta = distance * :math.sin(whereami.heading)
+    %{whereami | x: whereami.x + x_delta, y: whereami.y + y_delta, encoder_counts_left: count_left, encoder_counts_right: count_right}
+  end
+  defp updated_whereami(whereami, {count_left, count_right}, {diff_left, diff_right}) do
+    {x_delta, y_delta, heading_delta} = deltas(whereami.heading, diff_left, diff_right)
+
+    %WhereAmI{
+      x: whereami.x + x_delta,
+      y: whereami.y + y_delta,
+      heading: clamp_heading(whereami.heading + heading_delta),
+      encoder_counts_left: count_left,
+      encoder_counts_right: count_right,
+    }
   end
 
   defp clamp_heading(heading) do
@@ -41,6 +35,20 @@ defmodule Recruitbot.DeadReckoner do
       true ->
         heading
     end
+  end
+
+  # lovingly borrowed from http://www.seattlerobotics.org/encoder/200010/dead_reckoning_article.html
+  defp deltas(heading, diff_left, diff_right) do
+    cos_current = :math.cos(heading)
+    sin_current = :math.sin(heading)
+    dist_left = encoder_counts_to_distance(diff_left)
+    dist_right = encoder_counts_to_distance(diff_right)
+    right_minus_left = dist_right - dist_left
+    expr1 = @axle_length * (dist_right + dist_left) / 2.0 / (right_minus_left)
+    x_delta = expr1 * (:math.sin(right_minus_left / @axle_length + heading) - sin_current)
+    y_delta = -1 * expr1 * (:math.cos(right_minus_left / @axle_length + heading) - cos_current)
+    heading_delta = right_minus_left / @axle_length
+    {x_delta, y_delta, heading_delta}
   end
 
   defp encoder_counts_to_distance(count) do
