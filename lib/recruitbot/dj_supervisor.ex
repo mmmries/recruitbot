b566f88c58004cab87716f6c2b1c7e94827a7030
hqmq <michael@riesd.com>
2016-08-25 22:44:35 -0500

    send sensor updates from DJSupervisor => Tracker and publish position updates

--- lib/recruitbot/dj_supervisor.ex
+++ lib/recruitbot/dj_supervisor.ex
@@ -1,6 +1,8 @@
 defmodule Recruitbot.DJSupervisor do
   use Supervisor
   require Logger
+  alias Recruitbot.{Pulsar,Tracker,Whisker}
+  alias Roombex.{DJ,WhereAmI}
 
   @sensors_to_publish [
     :battery_capacity,
@@ -25,9 +27,10 @@ defmodule Recruitbot.DJSupervisor do
 
     dj_opts = Keyword.put(dj_opts, :report_to, publisher_pid)
     children = [
-      worker(Roombex.DJ, [dj_opts, [name: :dj]]),
-      worker(Recruitbot.Pulsar, []),
-      worker(Recruitbot.Whisker, []),
+      worker(DJ, [dj_opts, [name: :dj]]),
+      worker(Pulsar, []),
+      worker(Tracker, []),
+      worker(Whisker, []),
     ]
     supervise(children, strategy: :one_for_all)
   end
@@ -36,6 +39,8 @@ defmodule Recruitbot.DJSupervisor do
     receive do
       {:roomba_status, roombex_sensors} ->
         Recruitbot.Endpoint.broadcast("roomba", "sensor_update", sensors_to_map(roombex_sensors))
+        whereami = Tracker.update(roombex_sensors)
+        Recruitbot.Endpoint.broadcast("roomba", "position_update", whereami_to_map(whereami))
       msg -> Logger.error("#{__MODULE__} Received unexpected message #{inspect msg}")
     end
     publish_roomba_updates
@@ -43,5 +48,9 @@ defmodule Recruitbot.DJSupervisor do
 
   defp sensors_to_map(sensors) do
     Map.take(sensors, @sensors_to_publish)
+  end
+
+  defp whereami_to_map(%WhereAmI{x: x, y: y, heading: heading}) do
+    %{x: x, y: y, heading: heading}
   end
 end
